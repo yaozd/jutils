70e3d01c4f728c87c495076e7763502eff058471
yzd <tedijoteamo@163.com>
2017-04-24 13:59:52 +0800

    Java如何将Exception.printStackTrace()转换为String输出

---
+++ src/main/java/com/JUtils/exception/ExceptionUtils.java
@@ -0,0 +1,35 @@
+package main.java.com.JUtils.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Java如何将Exception.printStackTrace()转换为String输出
+ * http://lijun0349-163-com.iteye.com/blog/1073356
+ * Created by zd.yao on 2017/4/24.
+ */
+public class ExceptionUtils {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            String[] arr = {"111", "222"};
+            arr[2] = "fff";
+        } catch (Exception e) {
+            String info = getErrorInfoFromException(e);
+            System.out.println(info);
+        }
+    }
+
+    public static String getErrorInfoFromException(Exception e) {
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            return "\r\n" + sw.toString() + "\r\n";
+        } catch (Exception e2) {
+            return "bad getErrorInfoFromException";
+        }
+    }
+}
